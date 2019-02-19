f9940aac99ef92b9c0caf9b8975f51aae1255183
Pato-Note <Pato-Note@DESKTOP-PBGG4EG>
2019-02-18 22:05:48 -0300

    avanzando 1

--- src/Colaborador.java
+++ src/Colaborador.java
@@ -1,3 +1,46 @@
 public class Colaborador {
     
+    private String rut;
+    private String nombre;
+    private byte edad;
+    private int sueldo;
+
+    public Colaborador(String rut, String nombre, byte edad, int sueldo) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
 }
--- src/Empresa.java
+++ src/Empresa.java
@@ -1,3 +1,52 @@
+import java.util.ArrayList;
 public class Empresa {
+    
+    private String nombre;
+    private ArrayList<Colaborador> colaboradores;
+    
+    public Empresa(){
+        this.colaboradores= new ArrayList();
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public ArrayList<Colaborador> getColaboradores() {
+        return colaboradores;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public boolean buscarPorRut(String rut){
+        for(Colaborador x:colaboradores){
+            if(x.getRut().equals(rut)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean incorporarColaborador(Colaborador c){
+        if(buscarPorRut(c.getRut())){
+            return false;
+        }
+        colaboradores.add(c);
+        return true;
+    }
+    
+    public ArrayList<Colaborador> buscarColaboradoresPorSueldo(int minimo,int maximo){
+        ArrayList<Colaborador> x = new ArrayList();
+        for(Colaborador c:colaboradores){
+            if(c.getSueldo()>minimo){
+                if(c.getSueldo()<maximo){
+                    x.add(c);
+                }
+            }
+        }
+        return x;
+    }
     
 }
